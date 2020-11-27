70786d1ee2fce34ec97d45b397ae5b432c6e5c5b
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2020-11-27 15:44:06 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+
+const mocksString = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nam reprehenderit repellat provident fugit? Modi est eaque rerum commodi perspiciatis, neque accusamus cupiditate incidunt debitis, quam esse aliquam veniam dolorem!Est dolore repellat voluptatum minus. Doloribus quo sunt nihil ipsa quaerat. Consectetur saepe asperiores porro atque ex nostrum facilis nihil cumque. Dolorem velit, nostrum voluptates aperiam dolor veritatis consectetur culpa?';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea name="editor" cols="30" rows="10">{mocksString}</textarea>
     </div>
   );
 }
