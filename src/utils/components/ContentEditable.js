02e438da2ec41ff6de3c11d05ac9e8f936a10f38
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2020-11-29 12:58:12 +0100

    accessing DOM vernios (not buggie)

--- src/utils/components/ContentEditable.js
+++ src/utils/components/ContentEditable.js
@@ -1,36 +1,15 @@
 import React from 'react';
-import classnames from 'classnames';
 import Editable from 'react-contenteditable'
 
-function ContentEditable ({input, text, ranges, replaceRange, selection}) {
+function ContentEditable ({text, editText, showCurrent, sanitize}) {
 
-  const renderText = (text, ranges) => {
-    let chunk = '';
-    const res = [];
-    let current = ranges[0];
-  
-    for (let i = 0; i < text.length; i++) {
-      chunk += text[i];
-      
-      if (ranges[i+1] !== current) {
-        const span = `<span key="${i}" class="${classnames(ranges[i])}">${chunk}</span>`
-        res.push(span);
-        
-        current = ranges[i+1];
-        chunk = '';
-      }
-    }
-    chunk && res.push(chunk);
-    return res;
-  };
-  
   return (
       <Editable
-        innerRef={input}
         className="editor" 
-        html={renderText(text, ranges).join('')}
-        onChange={replaceRange}
-        onSelect={selection}
+        html={text}
+        onChange={editText}
+        onSelect={showCurrent}
+        onBlur={sanitize}
       /> 
   );
 }
--- src/utils/components/Editor.js
+++ src/utils/components/Editor.js
@@ -1,101 +1,55 @@
-import { useState, useRef, useEffect, useCallback } from 'react';
-import { getSelectionEnd, getSelectionStart, getCaretPosition } from "../caret";
+import { useRef, useState } from 'react';
+import sanitizeHtml from "sanitize-html";
+
 import StyleButton from "./StyleButton";
 import ContentEditable from "./ContentEditable";
 
 
-// Call to fill the first time the storage
-
-// function setMockStorage(text, ranges) {
-//   window.localStorage.setItem('text',JSON.stringify({text, ranges}))
-// }
-const mocksString = 'A string that says something in English so I can tell better if it is breaking or not';
-const mockRanges = new Array(mocksString.length).fill('');
-
-// setMockStorage(mocksString,mockRanges); 
-
 function Editor() {
-  // const [text, setText] = useState(mocksString);
-  const text = useRef(mocksString);
-  const [ranges, setRanges] = useState(mockRanges);
-  const [style, setStyle] = useState({})
-  const input = useRef();
+  const text = useRef(window.localStorage.getItem('text'));
+  const [styles, setStyles] = useState({})
 
-  // useEffect(() => {
-    // const prevText = JSON.parse(window.localStorage.getItem('text'));
-    // if (!prevText) return;
-    // setText(prevText.text)
-    // setRanges(prevText.ranges)
-  // },[])
-
-  // useEffect(() => {
-    // window.localStorage.setItem('text',JSON.stringify({text, ranges}))
-  // }, [text, ranges])
+  function editText ({ target }) {
+    text.current = target.value;
+    updateStorage();
+  }
 
   function setStyleSelection (style) {
-    const start = getSelectionStart(input.current)
-    const end = getSelectionEnd(input.current);
-
-    const newRanges = ranges[start] && ranges[start].includes(style)
-      ? removeRanges(ranges, start, end, style)
-      : addRanges(ranges, start, end, style)
-    if (!newRanges) return console.error('Error', 'start:', start, 'end:', end)
-    setRanges([...newRanges]);
-  } 
-
-  function addRanges (ranges, s, e, style) {
-    for (let i = s; i < e && i < ranges.length; i++) {
-      const styles = ranges[i].split(' ');
-      const {bold='', italic='', underline=''} = styles.reduce((acc, styleName) => {
-        acc[styleName] = styleName;
-        return acc;
-      },{[style]:style})
-  
-      ranges[i] = `${bold} ${italic} ${underline}`;
-    }
-  
-    return ranges;
-  }
-  
-  function removeRanges (ranges, s, e, style) {
-    for (let i = s; i < e && i < ranges.length; i++) {
-      const styles = ranges[i].split(' ').filter(styleName => styleName !== style);
-      ranges[i] = styles.join(' ');
-    }
-  
-    return ranges;
+    document.execCommand(style);
+    setStyles(styles => ({...styles, [style]:true}))
+    updateStorage();
   }
 
-  const selection = useCallback(() => {
-    let index = getCaretPosition(input.current);
-    if (!ranges[index]) return setStyle({});
-    const styles = ranges[index].split(' ');
-    setStyle(styles.reduce((acc, key) => {
-      acc[key] = true;
-      return acc;
-    },{}))
-  },[])
+  function updateStorage() {
+    window.localStorage.setItem('text', text.current)
+  }
 
-  const replaceRange = useCallback(({ target }) => {
-    let i = getCaretPosition(input.current) - 1;
-    if (ranges[i] === undefined) return;
-    setRanges(ranges => {
-      const style = ranges[i-1] || '';
-      if (target.value.length > ranges.length) ranges.splice(i, 0, style);
-      else ranges.splice(i, 1);
-      return ranges;
-    })
-    text.current = input.current.innerText;
-  },[])
+  function sanitize () {
+    text.current = sanitizeHtml(text.current, { allowedTags: ["b", "i", "u"] });
+  };
+
+  function showCurrent () {
+    const currentStyles = {
+      bold: document.queryCommandState('bold'),
+      italic: document.queryCommandState('italic'),
+      underline: document.queryCommandState('underline'),
+    }
+    if (
+      currentStyles.bold !== styles.bold &&
+      currentStyles.bold !== styles.bold &&
+      currentStyles.bold !== styles.bold ) {
+        setStyles(currentStyles);
+    }
+  }
 
   return (
     <>
       <div className="buttons">
-        <StyleButton setStyleSelection={setStyleSelection} styleName="bold" icon="B" active={style.bold}/> 
-        <StyleButton setStyleSelection={setStyleSelection} styleName="italic" icon="I" active={style.italic}/> 
-        <StyleButton setStyleSelection={setStyleSelection} styleName="underline" icon="U" active={style.underline}/> 
+        <StyleButton setStyleSelection={setStyleSelection} styleName="bold" icon="B" active={styles.bold}/> 
+        <StyleButton setStyleSelection={setStyleSelection} styleName="italic" icon="I" active={styles.italic}/> 
+        <StyleButton setStyleSelection={setStyleSelection} styleName="underline" icon="U" active={styles.underline}/> 
       </div>
-      <ContentEditable text={text.current} ranges={ranges} selection={selection} replaceRange={replaceRange} input={input}/>
+      <ContentEditable text={text.current} editText={editText} sanitize={sanitize} showCurrent={showCurrent}/>
     </>
   );
 }
